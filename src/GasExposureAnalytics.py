dc09417b95b776cca276416d5a6f14f74240ea54
John Segrave <john.segrave@ie.ibm.com>
2020-09-14 17:23:33 +0100

    Added testing with CSV data. Applied PEP 8 style guide. Other small fixups.

--- src/GasExposureAnalytics.py
+++ src/GasExposureAnalytics.py
@@ -1,83 +1,102 @@
-import pandas as pd
-import numpy as np
-import sqlalchemy
 import json
 import os
-
-# Constants / definitions (not used as mutable variables)
-CONST_sensor_log_tablename : Final = 'firefighter_sensor_log'
-CONST_analytics_tablename : Final = 'firefighter_status_analytics'
-CONST_firefighter_id_col : Final = 'firefighter_id'
-CONST_firefighter_id_col_type : Final = sqlalchemy.types.VARCHAR(length=20) # mySQL needs to be told this explicitly in order to generate correct SQL
-CONST_timestamp_col : Final = 'timestamp_mins'
+import numpy as np
+import pandas as pd
+import sqlalchemy
 
 
-class gasExposureAnalytics(object):
+# Constants / definitions
+SENSOR_LOG_TABLE = 'firefighter_sensor_log'
+ANALYTICS_TABLE = 'firefighter_status_analytics'
+FIREFIGHTER_ID_COL = 'firefighter_id'
+FIREFIGHTER_ID_COL_TYPE = sqlalchemy.types.VARCHAR(length=20) # mySQL needs to be told this explicitly in order to generate correct SQL
+TIMESTAMP_COL = 'timestamp_mins'
 
-    # Get configuration (1) currenty supported/active gases and (2) gas exposure windows and limits (e.g. from AEGL).
-    def __init__(self):
-        with open('windows_and_limits_config.json', 'r') as file:
-            self.windows_and_limits = json.load(file)
+
+class GasExposureAnalytics(object):
+
+
+    def __init__(self, list_of_csv_files=None):
+
+        # Get configuration (1) currently supported/active gases and (2) gas exposure windows and limits (e.g. from AEGL).
+
+        # windows_and_limits   : A list detailing every supported time-window over which to calcuate the time-weighted
+        #                        average (label, number of minutes and gas limit gauges for each window).
+        with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src/windows_and_limits_config.json')) as file:
+            self._windows_and_limits = json.load(file) # todo: how do paths work?
             file.close()
-        with open('supported_gases_config.json', 'r') as file:
-            self.supported_gases = json.load(file)
+
+        
+        # supported_gases   : The list of gases that Prometeo devices currently have sensors for.
+        #                     To automatically enable analytics for new gases, simply add them to this list.
+        with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src/supported_gases_config.json')) as file:
+            self._supported_gases = json.load(file) # todo: how do paths work?
             file.close()
+
 
         # db identifiers
         SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://"+os.getenv('MARIADB_USER')
                                     +":"+os.getenv("MARIADB_PASSWORD")
                                     +"@"+os.getenv("MARIADB_HOST")
-                                    +":"+int(os.getenv("MARIADB_PORT"))
+                                    +":"+str(os.getenv("MARIADB_PORT"))
                                     +"/prometeo")
-
         metadata=sqlalchemy.MetaData(SQLALCHEMY_DATABASE_URI)
-        self.db_engine = metadata.bind
+        self._db_engine = metadata.bind
 
-    # Query the last 8 hours of sensor logs
-    def get_block_of_sensor_readings(window_end, from_db=True) :
+
+        # By default, the analytics will run from a database.
+        self._from_db = True
+        # For testing, they can also be run from a set of CSV files.
+        if list_of_csv_files is not None : 
+            self._from_db = False
+            print("Taking sensor readings *** from CSV ***") # todo: logfile?
+            dataframes = []
+            for csv_file in list_of_csv_files : 
+                df = pd.read_csv(csv_file, parse_dates=[TIMESTAMP_COL], index_col = TIMESTAMP_COL) # todo: how do paths work for testing?
+                dataframes.append(df)
+            self._sensor_log_from_csv_df = pd.concat(dataframes)
+
+
+    # Query the last N hours of sensor logs, where N is the longest TWA window length. This
+    # method assumes that sensor data is stored against a minute-floor timestamp key - i.e.
+    # that a sensor value captured at 12:00:05 is stored against a timestamp of 12:00:00.
+    # window_end : The datetime from which to look back when reading the sensor logs (e.g. 'now').
+    def _get_block_of_sensor_readings(self, window_end) :
         
         # very important: everything in the system needs to synchronise to minute-boundaries
         window_end = window_end.floor(freq='min')
-        longest_window = max([window['mins'] for window in self.windows_and_limits])
+        longest_window = max([window['mins'] for window in self._windows_and_limits])
         window_start = window_end - pd.Timedelta(minutes = longest_window) # e.g. 8hrs ago
 
         sensor_log_df = pd.DataFrame()
-        # Get from database
-        if from_db :
+        if self._from_db :
+            # Get from database
             # Three ways to think about, depending on how time is being reported and whether this is running 'live' or in testing mode
             # 1. Get a RANGE
             # todo: check if this is inclusive or right/left exclusive - may be the source of the 'last TWA calculation of the day' issue?
-            sql = "SELECT * FROM " + CONST_sensor_log_tablename + " where " + CONST_timestamp_col + " between '" + window_start.isoformat() + "' and '" + window_end.isoformat() + "'"
+            sql = "SELECT * FROM " + SENSOR_LOG_TABLE + " where " + TIMESTAMP_COL + " between '" + window_start.isoformat() + "' and '" + window_end.isoformat() + "'"
             # 2. Get the LAST 8 HOURS (using the app server's perception of 'now')
-            # sql = "SELECT * FROM " + CONST_sensor_log_tablename + " where " + CONST_timestamp_col + " > '" + window_start.isoformat() + "'"
+            # sql = "SELECT * FROM " + SENSOR_LOG_TABLE + " where " + TIMESTAMP_COL + " > '" + window_start.isoformat() + "'"
             # 3. Get the LAST 8 HOURS (using the database's perception of 'now')
-            # sql = "SELECT * FROM " + CONST_sensor_log_tablename + " where " + CONST_timestamp_col + " > (DATE_SUB(NOW(), INTERVAL 8 HOUR))
-            sensor_log_df = pd.read_sql_query(sql, self.db_engine, parse_dates=[CONST_timestamp_col], index_col=CONST_timestamp_col).sort_index()
+            # sql = "SELECT * FROM " + SENSOR_LOG_TABLE + " where " + TIMESTAMP_COL + " > (DATE_SUB(NOW(), INTERVAL 8 HOUR))
+            sensor_log_df = pd.read_sql_query(sql, self._db_engine, parse_dates=[TIMESTAMP_COL], index_col=TIMESTAMP_COL).sort_index()
 
-        # Get from local CSV files - useful when testing (using local CSV data from the February test)
         else :
-            global sensor_log_from_csv_df
-            if 'sensor_log_from_csv_df' not in globals() :
-                print('Reading CSV files from disk...')
-                day1_df = pd.read_csv('~/code/prometeo/js/2020-02-10 data prescribed burn V2_FORMAT.csv', parse_dates=[CONST_timestamp_col], index_col = CONST_timestamp_col)
-                day2_df = pd.read_csv('~/code/prometeo/js/2020-02-11 data prescribed burn V2_FORMAT.csv', parse_dates=[CONST_timestamp_col], index_col = CONST_timestamp_col)
-                sensor_log_from_csv_df= pd.concat([day1_df, day2_df])
+            # Get from local CSV files - useful when testing (e.g. using CSV data from the February test)
+            sensor_log_df = self._sensor_log_from_csv_df.loc[window_start:window_end,:].sort_index().copy()
 
-            print("Reading block of sensor readings *** from CSV ***")
-            sensor_log_df = sensor_log_from_csv_df.loc[window_start:window_end,:].sort_index().copy()
+        if (sensor_log_df.empty) : print("No 'live' sensor records found in range ["+str(window_start)+" to "+str(window_end)+"]") # todo: logfile?
 
-        if (sensor_log_df.empty) : print("No 'live' sensor records found in range ["+str(window_start)+" to "+str(window_end)+"]") # todo: write to a logfile somewhere?
         return sensor_log_df
 
 
     # Given up to 8 hours of data, calculates the time-weighted average and limit gauge (%) for all firefighters, for all supported gases, for all configured time periods.
     # sensor_log_chunk_df  : A time-indexed dataframe covering up to 8 hours of sensor data for all firefighters, for all supported gases. Requires firefighterID and supported gases as columns.
-    # self.windows_and_limits   : A list detailing every supported time-window over which to calcuate the time-weighted average (label, number of minutes and gas limit gauges for each window).
-    # current_timestamp    : The timstamp for which to calculate time-weighted averages.
-    def calculate_TWA_and_gauge_for_all_firefighters(sensor_log_chunk_df, self.windows_and_limits, current_timestamp) :
+    # current_timestamp    : The timstamp for which to calculate time-weighted averages (e.g. 'now').
+    def _calculate_TWA_and_gauge_for_all_firefighters(self, sensor_log_chunk_df, current_timestamp) :
 
         # We'll be processing the windows in descending order of length (mins) 
-        windows_in_descending_order = sorted([window for window in self.windows_and_limits], key=lambda window: window['mins'], reverse=True)
+        windows_in_descending_order = sorted([window for window in self._windows_and_limits], key=lambda window: window['mins'], reverse=True)
         longest_window_mins = windows_in_descending_order[0]['mins'] # topmost element in the windows
         longest_window_timedelta = pd.Timedelta(minutes = longest_window_mins)
         slice_correction = pd.Timedelta(minutes = 1) # subtract 1 min because pandas index slicing is __inclusive__ and we don't want 11 samples in a 10 min average
@@ -101,7 +120,7 @@ class gasExposureAnalytics(object):
         resample_timedelta = pd.Timedelta(minutes = 1)
         longest_window_cleaned_df = (longest_window_df
                                     .sort_index()
-                                    .groupby(CONST_firefighter_id_col, group_keys=False)
+                                    .groupby(FIREFIGHTER_ID_COL, group_keys=False)
                                     .resample(resample_timedelta).backfill(limit=3)
                                     )
         
@@ -123,7 +142,7 @@ class gasExposureAnalytics(object):
             latest_sensor_readings_df = (longest_window_cleaned_df
                                         .loc[[common_key],:] # the current timeslice
                                         .reset_index()
-                                        .set_index([CONST_firefighter_id_col, CONST_timestamp_col])  # match up the indexing to the TWA dataframes
+                                        .set_index([FIREFIGHTER_ID_COL, TIMESTAMP_COL])  # match up the indexing to the TWA dataframes
                                         )
             # Store for merging later on
             latest_sensor_readings = [latest_sensor_readings_df] 
@@ -138,14 +157,14 @@ class gasExposureAnalytics(object):
             window_duration_label = window['label']
             
             # get a slice for this specific window, for all supported gas sensor readings (and excluding anll other columns)
-            analytic_cols = self.supported_gases + [CONST_firefighter_id_col]
+            analytic_cols = self._supported_gases + [FIREFIGHTER_ID_COL]
             window_df = (longest_window_cleaned_df
                         .loc[(current_timestamp - window_timedelta + slice_correction).isoformat():current_timestamp.isoformat(), analytic_cols])
             
             # If the window is empty, we still need to append it to the 'everything' dataframe
             if (window_df.empty) :
                 # # Update column titles - add the time period over which we're averaging, so we can merge dataframes later without column name conflicts
-                # empty_df = window_df.reset_index().set_index([CONST_firefighter_id_col, CONST_timestamp_col])
+                # empty_df = window_df.reset_index().set_index([FIREFIGHTER_ID_COL, TIMESTAMP_COL])
                 # empty_twa_df = empty_df.add_suffix('_twa_' + window_duration_label)
                 # empty_gauge_df = empty_df.add_suffix('_gauge_' + window_duration_label)
                 # # Now save the results from this time window as a single merged dataframe (TWAs and Limit Gauges)
@@ -153,7 +172,7 @@ class gasExposureAnalytics(object):
                 continue # TODO: can we get away with just leaving the empty window out? Depends on how the DB write will work
 
             # Sanity check that there's never more data in the window than there should be (1 record per minute per FF, max)
-            assert(window_df.groupby(CONST_firefighter_id_col).size().max() <= window_mins)
+            assert(window_df.groupby(FIREFIGHTER_ID_COL).size().max() <= window_mins)
 
             # Confirm there's enough info to calculate with, using information about gaps obtained before resampling
             # todo: check here whether to calculate or return NaN, based on max length of dropouts / disconnected periods.
@@ -164,17 +183,17 @@ class gasExposureAnalytics(object):
             window_sample_count = window_timedelta / resample_timedelta
             # Use .sum() and divide by a fixed-time denominator for each window.
             # Don't use .mean() - it has a variable denominator (however many datapoints it happens to have), which over-estimates exposure during startup.
-            window_twa_df = (window_df.groupby(CONST_firefighter_id_col).sum() / float(window_sample_count))
+            window_twa_df = (window_df.groupby(FIREFIGHTER_ID_COL).sum() / float(window_sample_count))
             # Give the window its index key. Note: since records are quantized to floor(minute), they need a 1 minute arrival buffer, hence the correction
-            window_twa_df[CONST_timestamp_col] = common_key
+            window_twa_df[TIMESTAMP_COL] = common_key
 
             # Prepare the results for limit gauges and merging
-            window_twa_df = window_twa_df.reset_index().set_index([CONST_firefighter_id_col, CONST_timestamp_col])
+            window_twa_df = window_twa_df.reset_index().set_index([FIREFIGHTER_ID_COL, TIMESTAMP_COL])
             
             # Calculate gas limit gauge - percentage over / under the calculated TWA values
             # (must compare gases in the same order as the dataframe columns)
-            limits_in_column_order = [float(window['gas_limits'][gas]) for gas in window_twa_df.columns if gas in self.supported_gases]
-            window_gauge_df = (window_twa_df * 100 / limits_in_column_order).round(0).astype(int) # whole integer percentage, shorter to send over JSON
+            limits_in_column_order = [float(window['gas_limits'][gas]) for gas in window_twa_df.columns if gas in self._supported_gases]
+            window_gauge_df = (window_twa_df * 100 / limits_in_column_order).round(0).astype(int) # whole integer percentage, don't need floats
 
             # Update column titles - add the time period over which we're averaging, so we can merge dataframes later without column name conflicts
             window_twa_df = window_twa_df.add_suffix('_twa_' + window_duration_label)
@@ -189,7 +208,7 @@ class gasExposureAnalytics(object):
         # If there were no latest sensors readings to merge, then just set all the sensor cols to null
         if not latest_sensor_readings :
             # Do this for all sensor columns, except the two keys
-            for col in list(set(sensor_log_chunk_df.columns) - set([CONST_firefighter_id_col, CONST_timestamp_col])) :
+            for col in list(set(sensor_log_chunk_df.columns) - set([FIREFIGHTER_ID_COL, TIMESTAMP_COL])) :
                 everything_for_1_min_df[col] = None # todo: works, but would prefer a more "pandas-y" way of achieving this with a multi-level index...
         
         # makes it slightly easier to read
@@ -200,28 +219,23 @@ class gasExposureAnalytics(object):
 
     # This is 'main' - runs all of the core analytics for Prometeo in a given minute.
     # time : The datetime for which to calculate sensor analytics. Defaults to 'now'.
-    # from_db : Utility flag for unit testing - defaults to running against the database.
-    #           Setting from_db=False causes tests to be run against local CSV files instead.
     # commit : Utility flag for unit testing - defaults to committing analytic results to
     #          the database. Setting commit=False prevents unit tests from writing to the database.
-    def run_analytics (time=pd.Timestamp.now(), from_db=True, commit=True) :
+    def run_analytics (self, time=pd.Timestamp.now(), commit=True) :
 
         # Read the max window block from the database - todo: ensure this a non-blocking read (not read-for-update)
         # We can make this more performant, but at the start "make it correct, then write the tests, THEN optimise (with a safety net)" 
         # Also - this is robust to dropouts - querying 'everything known' from the sensor log ensures that
         # analytic processing can include any delayed records that have since arrived.
-        sensor_log_df = get_block_of_sensor_readings(time, from_db)
+        sensor_log_df = self._get_block_of_sensor_readings(time)
 
         # Stop if there's no data (e.g. (1) after the system is booted but before any records have come in. (2) 8+ hours after an event
         if (sensor_log_df.empty) : return    
         
         # Work out all the time-weighted averages and corresponding limit gauges for all firefighters, all limits and all gases.
-        analytics_df = calculate_TWA_and_gauge_for_all_firefighters(sensor_log_df, self.windows_and_limits, time)
+        analytics_df = self._calculate_TWA_and_gauge_for_all_firefighters(sensor_log_df, time)
 
         if commit :
-            analytics_df.to_sql(CONST_analytics_tablename, self.db_engine, if_exists='append', dtype={CONST_firefighter_id_col:CONST_firefighter_id_col_type})
+            analytics_df.to_sql(ANALYTICS_TABLE, self._db_engine, if_exists='append', dtype={FIREFIGHTER_ID_COL:FIREFIGHTER_ID_COL_TYPE})
 
-        # dicts are easy to return as JSON too
-        # analytics_df_dict = analytics_df.reset_index().to_json(orient='records', indent=2)
-        
         return analytics_df
--- src/core_decision_flask_app.py
+++ src/core_decision_flask_app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, Response, jsonify
 from flask_restplus import Api, Resource, fields, reqparse
 from flask_cors import CORS, cross_origin
-import os
-from gasExposureAnalytics import gasExposureAnalytics
+import pandas as pd
+from GasExposureAnalytics import GasExposureAnalytics
 
 
 # The application
@@ -10,17 +11,17 @@ app = Flask(__name__)
 CORS(app)
 
 api_prometeo_analytics = Api(app, version='1.0', title="Calculates Time-Weighted Average exposures and exposure-limit status 'gauges' for all firefighters for the last minute.", validate=False)
-ns = api_prometeo_analytics.namespace('gasExposureAnalytics', 'Calculates core Prometeo analytics')
+ns = api_prometeo_analytics.namespace('GasExposureAnalytics', 'Calculates core Prometeo analytics')
 
-# The API does not require any input data, but may optionally accept a list of Firefighter ID's as prompts to check for recently-received sensor records.
-model_input = api_prometeo_analytics.model('Enter the data:', {'firefighter_ids': fields.List(fields.String(description='Firefighter IDs'))})
+# The API does not require any input data. Once called, it will retrieve the latest data from the database.
+model_input = api_prometeo_analytics.model('Enter the data:', {'todo': fields.String(description='todo')})
 
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default to 8080
 port = int(os.getenv('PORT', 8080))
 
 # We initialize the prometeo Analytics engine.
-perMinuteAnalytics = gasExposureAnalytics()
+perMinuteAnalytics = GasExposureAnalytics()
 
 
 # The ENDPOINT
@@ -36,11 +37,15 @@ class prometeo_analytics(Resource):
         args = parser.parse_args()
 
         # Run all of the core analytics for Prometeo for a given minute.
-        limits_and_gauges = perMinuteAnalytics.run_analytics(list(args["firefighter_ids"]))
-        # limits_and_gauges_df, limits_and_gauges_json = perMinuteAnalytics.run_analytics(list(args["firefighter_ids"]))
+        limits_and_gauges_for_all_firefighters_df = perMinuteAnalytics.run_analytics()
+
+        # Return the limits and status gauges as json
+        if limits_and_gauges_for_all_firefighters_df is None :
+            return None
         
-        # # We return the status in a json format
-        # return jsonify({"Status": status})
+        return limits_and_gauges_for_all_firefighters_df.to_json(orient='index')
+        
+        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=False)  # deploy with debug=False
